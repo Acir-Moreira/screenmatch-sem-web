e0b5e476ddec22ceb0f1a9b30387a9bc289ba54e
Acir Moreira <acir.jmoreira@gmail.com>
2024-02-13 00:15:02 -0300

    Primeito Commit - Versão Inicial

---
+++ src/main/java/br/com/acir/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,28 @@
+package br.com.acir.screenmatch;
+
+import br.com.acir.screenmatch.model.DadosSerie;
+import br.com.acir.screenmatch.service.ConsumoApi;
+import br.com.acir.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.sound.midi.SysexMessage;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=8eb03852");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
